ad26b56cb6a731886a64e1ae49c653616f14f9e0
Quang Luong <qualuo@gmail.com>
2022-12-17 01:24:01 +0100

    Camera smoothing and clamp

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    // The target to follow
+    public Transform target;
+
+    // The bounds of the rectangle to clamp the camera within
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate() {
+        Vector3 cameraPosition = transform.position;
+
+        // Calculate the new position of the camera based on the target's position
+        cameraPosition = Vector3.SmoothDamp(cameraPosition, target.position, ref velocity, smoothTime);
+
+        cameraPosition.x = Mathf.Clamp(cameraPosition.x, minBounds.x, maxBounds.x);
+        cameraPosition.y = Mathf.Clamp(cameraPosition.y, minBounds.y, maxBounds.y);
+        cameraPosition.z = -10;
+
+        // Update the camera's position with the clamped values
+        transform.position = cameraPosition;
+    }
+}
